769e769af7e3781b83aaef54bd2f18e30ea9cc81
wladimirgrf <wladimir.code@gmail.com>
2023-10-08 10:10:38 -0300

    Controller de listagem de perguntas

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from './auth/auth.module'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 
 @Module({
   imports: [
@@ -21,6 +22,7 @@ import { CreateQuestionController } from './controllers/create-question.controll
     CreateAccountController,
     AuthenticateController,
     CreateQuestionController,
+    FetchRecentQuestionsController,
   ],
   providers: [PrismaService],
 })
