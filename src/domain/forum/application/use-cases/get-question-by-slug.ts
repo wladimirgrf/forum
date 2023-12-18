fb7376b6e2d7d561d85cd35d076d439d263d89cc
wladimirgrf <wladimir.code@gmail.com>
2023-12-18 10:48:09 -0300

    Value Object detalhes da pergunta

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common'
 
 import { Either, left, right } from '@/core/either'
-import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
 import { ResourceNotFoundError } from '@/core/errors/resource-not-found-error'
+import { QuestionDetails } from '../../enterprise/entities/value-objects/question-details'
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
@@ -12,7 +12,7 @@ interface GetQuestionBySlugUseCaseRequest {
 type GetQuestionBySlugUseCaseResponse = Either<
   ResourceNotFoundError,
   {
-    question: Question
+    question: QuestionDetails
   }
 >
 
@@ -23,7 +23,7 @@ export class GetQuestionBySlugUseCase {
   async execute({
     slug,
   }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
-    const question = await this.questionsRepository.findBySlug(slug)
+    const question = await this.questionsRepository.findDetailsBySlug(slug)
 
     if (!question) {
       return left(new ResourceNotFoundError())
---
+++ src/domain/forum/enterprise/entities/value-objects/question-details.ts
@@ -0,0 +1,63 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { ValueObject } from '@/core/entities/value-object'
+import { Slug } from './slug'
+import { Attachment } from '../attachment'
+
+interface AuthorProps {
+  id: UniqueEntityID
+  name: string
+}
+
+export interface QuestionDetailsProps {
+  questionId: UniqueEntityID
+  author: AuthorProps
+  title: string
+  slug: Slug
+  content: string
+  attachments: Attachment[]
+  bestAnswerId?: UniqueEntityID | null
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class QuestionDetails extends ValueObject<QuestionDetailsProps> {
+  static create(props: QuestionDetailsProps) {
+    return new QuestionDetails(props)
+  }
+
+  get questionId() {
+    return this.props.questionId
+  }
+
+  get author() {
+    return this.props.author
+  }
+
+  get title() {
+    return this.props.title
+  }
+
+  get slug() {
+    return this.props.slug
+  }
+
+  get content() {
+    return this.props.content
+  }
+
+  get attachments() {
+    return this.props.attachments
+  }
+
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+}
