858742ce8cf2e7a29b538561fcb5c3b1e1393d7e
wladimirgrf <wladimir.code@gmail.com>
2023-12-25 20:55:24 -0300

    Implementando Cache com Redis

---
+++ src/infra/cache/redis/redis-cache-repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common'
+
+import { CacheRepository } from '../cache-repository'
+import { RedisService } from './redis.service'
+
+@Injectable()
+export class RedisCacheRepository implements CacheRepository {
+  constructor(private redis: RedisService) {}
+
+  async set(key: string, value: string): Promise<void> {
+    const _15_MINUTES = 60 * 15
+    await this.redis.set(key, value, 'EX', _15_MINUTES)
+  }
+
+  async get(key: string): Promise<string | null> {
+    return this.redis.get(key)
+  }
+
+  async delete(key: string): Promise<void> {
+    await this.redis.del(key)
+  }
+}
--- src/infra/cache/redis/redis.service.ts
+++ src/infra/cache/redis/redis.service.ts
@@ -1,7 +1,9 @@
-import { EnvService } from '@/infra/env/env.service'
-import { OnModuleDestroy } from '@nestjs/common'
+import { Injectable, OnModuleDestroy } from '@nestjs/common'
 import { Redis } from 'ioredis'
 
+import { EnvService } from '@/infra/env/env.service'
+
+@Injectable()
 export class RedisService extends Redis implements OnModuleDestroy {
   constructor(envService: EnvService) {
     super({
