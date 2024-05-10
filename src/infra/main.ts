d23154d744f8e4194caa0296a629ce35a5ab260f
wladimirgrf <wladimir.code@gmail.com>
2024-05-10 07:25:08 -0300

    docs: swagger

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,4 +1,7 @@
 import { NestFactory } from '@nestjs/core'
+import * as swaggerUi from 'swagger-ui-express'
+import * as YAML from 'yamljs'
+
 import { AppModule } from './app.module'
 import { EnvService } from './env/env.service'
 
@@ -7,6 +10,9 @@ async function bootstrap() {
 
   const envService = app.get(EnvService)
   const port = envService.get('PORT')
+
+  const swaggerDocument = YAML.load('./swagger.yaml')
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
   await app.listen(port)
 }
